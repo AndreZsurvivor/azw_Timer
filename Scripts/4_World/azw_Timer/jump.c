d432b7c7f4d9c2e1a62b13cb5acb99c80153d3ee
AndreZ <zwieback@crazyuniverse.net>
2024-06-27 20:08:45 +0200

    added comments and formatting

--- Scripts/4_World/azw_Timer/jump.c
+++ Scripts/4_World/azw_Timer/jump.c
@@ -1,17 +1,18 @@
 modded class PlayerBase
 {
 	protected ref Timer m_MyTimer; // declaring the timer as a protected strong reference
+	
 	override void OnJumpStart()
     {
         super.OnJumpStart();   // call the original jump function so we don't break stuff
 
 		if (!m_MyTimer)
-			m_MyTimer = new Timer();  //create an instance of the Timer class
+			m_MyTimer = new Timer();  //create an instance of the Timer class if it doesn't exist already
 
 		m_MyTimer.Run(5.0, this, "HelloWorld", NULL, true); // run the timer to call HelloWorld() every five seconds
     }
 
-	void HelloWorld() // function to spawn an object with a timed delay
+	void HelloWorld() // function that prints HelloWorld
 	{
 		Print("HelloWorld!");
 	}
